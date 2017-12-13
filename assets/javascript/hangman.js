4bb534b9d3019824f4e2332ef81c5f64b09bcbc5
Dawn Wages <dawn.wages@gmail.com>
2017-12-13 02:23:28 -0500

    Game logic almost done, now working on displaying HTML from DOM

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -32,6 +32,8 @@ var randomComputerPick;
 var successPicks = [];
 var failPicks = [];
 var allPicks = [];
+var computerWord = [];
+var displayWord = [];
 
 var computerPick = listBlackMovies[Math.floor(Math.random() * listBlackMovies.length)];
 console.log(computerPick);
@@ -40,39 +42,129 @@ console.log('-------------------');
 var upperComputerPick = computerPick.toUpperCase();
 console.log(upperComputerPick);
 console.log('-------------------');
+// Make computer pick an array
 
+
+
+console.log(computerWord);
+console.log('-------------------');
+console.log(displayWord);
+console.log('-------------------');
 
 var emptyCharacters = upperComputerPick.length;
 console.log(emptyCharacters);
 console.log('-------------------');
 
-var hangmanDiv = document.getElementById('Hangman');
-
-for (var i = 0; i < upperComputerPick.length; i++){
-	var newLetterDiv = document.createElement('div');
-
-	newLetterDiv.innerHTML = "_";
-
-	hangmanDiv.appendChild(newLetterDiv);
 
 
-}
+
+//Don't need this anymore if I create the array of elements for what will be displayed 
+//for (var i = 0; i < upperComputerPick.length; i++){
+// 	var newLetterDiv = document.createElement('div');
+
+// 	newLetterDiv.innerHTML = "_";
+
+// 	hangmanDiv.appendChild(newLetterDiv);
+
+
+// }
+
+console.log("where are the spaces "+ upperComputerPick.indexOf(" "));
+
+	for (var i =0; i < upperComputerPick.length; i++){
+		if( upperComputerPick[i] === ' '){
+			computerWord.push(upperComputerPick[i]);
+			displayWord.push(' ');
+			}
+		else {
+			computerWord.push(upperComputerPick[i]);
+			displayWord.push('_');
+		}
+	} 
+
+
 
 var userDisplay = document.getElementById("user-text")
 
 document.onkeyup = function(event) {
 	var userPick = event.key.toUpperCase();
-	userDisplay.textContent = event.key;
+	userDisplay.textContent = userPick;
 	console.log(event.key);
 	console.log('-------------------');
+	console.log(allPicks);
+
+	console.log("this is the index location of previous pick versus current user pick: " + allPicks.indexOf(userPick));
+	
+	var userPick = event.key.toUpperCase();
+	userDisplay.textContent = userPick;
+	console.log(event.key);
+	console.log('-------------------');
+	console.log(allPicks);
+
+	console.log("this is the index location of previous pick versus current user pick: " + allPicks.indexOf(userPick));
+	
+	
+
+	if (allPicks.indexOf(userPick) > 0){
+		alert("You've already picked this character.");
+	}
+	else{
+		var charSuccess = 0;
+		for (var i = 0; i < upperComputerPick.length; i++){
+			
+			if (userPick == upperComputerPick[i]){
+				charSuccess = charSuccess +1;
+				displayWord[i] = userPick;
+			}
+
+			else {
+				
+			}
+			
+		}
+		console.log("this is how many letters you've just matched successfully:" + charSuccess);
+		console.log(displayWord);
+		if (charSuccess > 1){
+			alert("Yass Queen, you matched "+charSuccess+" letters!");
+		}
+		else if (charSuccess > 0){
+			alert("Yass Queen, you matched "+charSuccess+" letter!");
+		}
+		else {
+			alert("Darn Girl, try again.");
+			failPicks.push(userPick);
+
+		}
+	}
 
 	allPicks.push(userPick);
-	console.log(allPicks);
-	console.log('-------------------');
-
+	console.log("these are failed picks : "+ failPicks);
 	index = upperComputerPick.indexOf(userPick);
-	console.log(index);
+	console.log("this is the location of the user pick in the secret word:" + index);
 	console.log('-------------------');
+	repeat = allPicks.indexOf(userPick)
+	console.log("this is the location of the user pick in all of the picks you've done before:" + repeat);
+
+
+
+
+
+
+	// for (var i = 0; i < upperComputerPick.length; i++){
+		
+		
+
+	// 	else {
+
+
+	// 	}
+
+	// }
+
+
+		
+		
+
 
 	// Add a statment to check to see if i've already guessed it-- doesn't work. try a loop.
 // 	if (allPicks.indexOf(userPick) > -1){
@@ -95,7 +187,43 @@ document.onkeyup = function(event) {
 // 			alert("Sorry Sis!");
 // 		}
 // 	}
+
+var hangDisplay = document.getElementById("Hangman");
+hangDisplay.innerHTML = "";
+	for (var i = 0; i < displayWord.length; i++){
+
+		var newLetterDiv = document.createElement('div');
+
+		newLetterDiv.innerHTML= displayWord[i];
+
+		hangDisplay.appendChild(newLetterDiv);
+	}
+
+
+
+}
+// document.onkeyup = function(event) {
+// function updateScores(){
+// 	document.querySelector("badGuess").innerHTML = failPicks;
 // }
+
+// updateScores();
+// }
+
 
 // use indexOf
 // returns an index location
+	// var hangmanDiv = document.getElementById('HangmanToo');
+
+	// function renderDisplay() {
+	// 	for (var i = 0; i < upperComputerPick.length; i++){
+	// 		var eachChar = document.createElement('div');
+	// 		eachChar.innerHTML = displayWord[i];
+	// 		hangmanDiv.appendChild(eachChar);
+	// 	}
+	// }
+
+
+
+
+	// renderDisplay();
