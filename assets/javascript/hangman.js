c95dfa8e0ab408db933fe3a3ffcf5d1abc51b1ee
Dawn Wages <dawn.wages@gmail.com>
2017-12-12 22:44:02 -0500

    making progress

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,50 +1,101 @@
-var car = {
-    make: "Honda",
-    model: "Fit",
-    color: "Blue Raspberry",
-    mileage: 3000,
-    isWorking: true,
-
-    driveToWork: function() {
-      alert("Old Mileage: " + this.mileage);
-      this.mileage = this.mileage + 8;
-      alert("New mileage: " + this.mileage);
-    },
-
-    driveAroundWorld: function() {
-      alert("Old Mileage: " + this.mileage);
-      this.mileage = this.mileage + 24000;
-      alert("New Mileage: " + this.mileage);
-      alert("Car needs a tuneup!");
-      this.isWorking = false;
-    },
-
-    getTuneUp: function() {
-      alert("Car is ready to go!");
-      this.isWorking = true;
-    },
-
-    honk: function() {
-      alert("Honk! Honk!");
-    }
-  };
+//HANGMAN GAME 
 
 
-  // How would we log...
+//User picks a category
 
-  // The car's make?
+//Computer randomly picks a word from that Category
 
-    console.log("My car's make is "+ car.make);
-    console.log("------------------------");
-  // The car's model?
-    console.log("My car's model is "+ car.model);
-    console.log("------------------------");
-  // The car's mileage?
-  console.log("My car's mileage is "+ car.mileage);
-  console.log("------------------------");
-  // How would we run the car's driveToWork method?
-  car.driveAroundWorld()
+//Computer displays empty space for each letter in the word choice
 
-  // How would we run the car's driveAroundWorld method?
+//User must pick a letter
 
-  // How would we run the getTuneUp method?
+//No you've already chosen that letter
+
+//No it's not a letter
+
+//Check if letter matches the word choice
+
+//Success it matches
+
+  //fills in a letter of the hangman game
+  //adds letter to a list of letters already chose  
+
+//No it doesn't match 
+  //alerts that you have chosen a letter that does not match
+  //logs letter into letters already chosen
+
+//Go back to step one
+
+
+var listBlackMovies = ["Do The Right Thing", "Shaft", "Waiting to Exhale", "Love Jones", "The Color Purple", "John Q", "Moonlight", "SuperFly"];
+var randomComputerPick; 
+var successPicks = [];
+var failPicks = [];
+var allPicks = [];
+
+var computerPick = listBlackMovies[Math.floor(Math.random() * listBlackMovies.length)];
+console.log(computerPick);
+console.log('-------------------');
+
+var upperComputerPick = computerPick.toUpperCase();
+console.log(upperComputerPick);
+console.log('-------------------');
+
+
+var emptyCharacters = upperComputerPick.length;
+console.log(emptyCharacters);
+console.log('-------------------');
+
+var hangmanDiv = document.getElementById('Hangman');
+
+for (var i = 0; i < upperComputerPick.length; i++){
+	var newLetterDiv = document.createElement('div');
+
+	newLetterDiv.innerHTML = "_";
+
+	hangmanDiv.appendChild(newLetterDiv);
+
+
+}
+
+var userDisplay = document.getElementById("user-text")
+
+document.onkeyup = function(event) {
+	var userPick = event.key.toUpperCase();
+	userDisplay.textContent = event.key;
+	console.log(event.key);
+	console.log('-------------------');
+
+	allPicks.push(userPick);
+	console.log(allPicks);
+	console.log('-------------------');
+
+	index = upperComputerPick.indexOf(userPick);
+	console.log(index);
+	console.log('-------------------');
+
+	// Add a statment to check to see if i've already guessed it-- doesn't work. try a loop.
+// 	if (allPicks.indexOf(userPick) > -1){
+
+// 		alert("You've already tried that.");
+// 		console.log('did you already try this?');
+// 		console.log(allPicks.indexOf(userPick));
+// 		console.log('-------------------');
+
+// 	}
+
+// 	else{
+
+// 		if (index > -1 ){
+// 			alert("Good Guess!");
+// 			successPicks.push(userPick);
+// 		}
+
+// 		else {
+// 			alert("Sorry Sis!");
+// 		}
+// 	}
+// }
+
+// use indexOf
+// returns an index location
